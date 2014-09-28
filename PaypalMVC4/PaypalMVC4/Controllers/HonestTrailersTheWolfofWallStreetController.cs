c047ac11ae97b722885515ff34989d7fa9e333ad
zulldesign <admin@zulldesign.ml>
2014-09-28 19:11:47 +0800

    first commit

---
+++ PaypalMVC4/PaypalMVC4/Controllers/HonestTrailersTheWolfofWallStreetController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PaypalMVC4.Controllers
+{
+    public class HonestTrailersTheWolfofWallStreetController : Controller
+    {
+        //
+        // GET: /HonestTrailersTheWolfofWallStreet/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ PaypalMVC4/PaypalMVC4/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PaypalMVC4.MvcApplication" Language="C#" %>
